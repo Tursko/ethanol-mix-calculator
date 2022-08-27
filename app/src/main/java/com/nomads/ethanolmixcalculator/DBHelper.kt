e2047e5601f36c73c8db09c331d54ce613010e0e
Tursko <taylor@stykocompany.com>
2022-08-27 17:11:40 -0500

    Create class for handling the local database.

---
+++ app/src/main/java/com/nomads/ethanolmixcalculator/DBHelper.kt
@@ -0,0 +1,52 @@
+package com.nomads.ethanolmixcalculator
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION)
+ {
+
+     override fun onCreate(db: SQLiteDatabase) {
+         val createQuery = ("CREATE TABLE " + TABLE_NAME + " (" +
+                 NAME_COL + " TEXT," +
+                 TANK_SIZE_COL + " REAL" + ")")
+
+         db.execSQL(createQuery)
+     }
+
+     override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+         onCreate(db)
+     }
+
+     fun addCar(name: String, tankSize: Double) {
+         // String-key class
+         val values = ContentValues()
+
+         values.put(NAME_COL, name)
+         values.put(TANK_SIZE_COL, tankSize)
+
+         val db = this.writableDatabase
+         db.insert(TABLE_NAME, null, values);
+
+         db.close()
+     }
+
+     fun getAllCars(): Cursor? {
+         val db = this.readableDatabase
+         return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+     }
+
+     companion object { // Static
+         private val DATABASE_NAME = "ETHANOL_MIX_DB"
+         private val DATABASE_VERSION = 1
+
+         var TABLE_NAME = "car_table"
+         val NAME_COL = "name"
+         val TANK_SIZE_COL = "tank_size"
+     }
+ }
--- app/src/main/java/com/nomads/ethanolmixcalculator/Volume.kt
+++ app/src/main/java/com/nomads/ethanolmixcalculator/Volume.kt
@@ -1,26 +1,6 @@
 package com.nomads.ethanolmixcalculator
 
 class Volume(val value: Double, val uom: UoM) {
-    companion object Converter {
-        const val litersPerGallon: Double = 3.785411784
-
-        fun convert(volume: Volume, newUom: UoM) : Volume {
-            return when (Pair(volume.uom, newUom)) {
-                Pair(UoM.Gallons, UoM.Liters) -> Volume(volume.value * litersPerGallon, UoM.Liters)
-                Pair(UoM.Liters, UoM.Gallons) -> Volume(volume.value / litersPerGallon, UoM.Gallons)
-                Pair(UoM.Gallons, UoM.Gallons) -> Volume(volume.value, UoM.Gallons)
-                Pair(UoM.Liters, UoM.Liters) -> Volume(volume.value, UoM.Liters)
-                else -> throw NotImplementedError()
-            }
-        }
-    }
-
-
-    enum class UoM {
-        Liters,
-        Gallons
-    }
-
 
     val asGallons: Volume
         get() = convert(this, UoM.Gallons)
@@ -59,5 +39,24 @@ class Volume(val value: Double, val uom: UoM) {
 
     operator fun div(scalar: Double) : Volume {
         return Volume(this.value / scalar, this.uom)
+    }
+
+    companion object Converter {
+        const val litersPerGallon: Double = 3.785411784
+
+        fun convert(volume: Volume, newUom: UoM) : Volume {
+            return when (Pair(volume.uom, newUom)) {
+                Pair(UoM.Gallons, UoM.Liters) -> Volume(volume.value * litersPerGallon, UoM.Liters)
+                Pair(UoM.Liters, UoM.Gallons) -> Volume(volume.value / litersPerGallon, UoM.Gallons)
+                Pair(UoM.Gallons, UoM.Gallons) -> Volume(volume.value, UoM.Gallons)
+                Pair(UoM.Liters, UoM.Liters) -> Volume(volume.value, UoM.Liters)
+                else -> throw NotImplementedError()
+            }
+        }
+    }
+
+    enum class UoM {
+        Liters,
+        Gallons
     }
 }
